564d99c37b21f5b3466a576308be6029ed02a706
wyattowalsh <wyattowalsh@gmail.com>
2024-12-03 07:06:23 -0500

    Remove custom webpack plugin configuration and TypeScript paths for simplification

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -34,19 +34,7 @@ const config: Config = {
   plugins: [
     'docusaurus-plugin-sass',
     [
-      '@docusaurus/plugin-ideal-image',
-      {
-        name: 'custom-webpack-plugin',
-        configureWebpack(config, isServer, utils) {
-          return {
-            resolve: {
-              alias: {
-                '@': path.resolve(__dirname, './src'),
-              },
-            },
-          };
-        },
-      },
+      '@docusaurus/plugin-ideal-image'
     ],
   ],
 
