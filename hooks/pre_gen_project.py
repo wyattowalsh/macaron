26007eb95373b528b7b3a40ac71f0f8ab6bd0800
Wyatt Walsh <wyattowalsh@gmail.com>
2024-12-03 15:58:31 -0500

    Add validation for project slug and update version in pyproject.toml

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -46,5 +46,13 @@ def validate_config():
         sys.exit(1)
 
 
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+module_name = '{{ cookiecutter.project_slug}}'
+
+if not re.match(MODULE_REGEX, module_name):
+    print('ERROR: The project slug (%s) is not a valid Python module name!' %
+          module_name)
+    sys.exit(1)
+
 if __name__ == "__main__":
     validate_config()
