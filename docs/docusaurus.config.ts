e322dbefd7bf7f651e88a7cd649bf70f59f73a7c
wyattowalsh <wyattowalsh@gmail.com>
2024-12-03 05:51:51 -0500

    Update Docusaurus configuration for production URL and base path

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://w4w.dev/',
+  url: 'https://macaron.w4w.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/macaron/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
