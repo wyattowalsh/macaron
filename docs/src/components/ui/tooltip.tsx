2c49cb8bbb165473fd85960797ae969750b9a84c
wyattowalsh <wyattowalsh@gmail.com>
2024-12-03 07:22:20 -0500

    Remove unnecessary line breaks in UI component files for cleaner code

--- docs/src/components/ui/tooltip.tsx
+++ docs/src/components/ui/tooltip.tsx
@@ -2,9 +2,6 @@ import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 import { cn } from '../../lib/utils'
-'
-
-  // Updated import path
 
 
 const TooltipProvider = TooltipPrimitive.Provider
